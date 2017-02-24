fc9598cafd45f25040f6331851adf53d9221b3e2
katerinachinnappan <katiachinnap@gmail.com>
2017-02-24 12:33:21 -0800

    KB , facts

---
+++ Phase2/src/Fact.h
@@ -0,0 +1,16 @@
+#ifndef FACT_H_INCLUDED
+#define FACT_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <map>
+#include "KB.h"
+class Fact{
+    public:
+        Fact();
+
+};
+
+
+
+
+#endif // FACT_H_INCLUDED
--- Phase2/src/KB.h
+++ Phase2/src/KB.h
@@ -1,6 +1,23 @@
-#pragma once
-
-class KB
-{
+#ifndef KB_H_INCLUDED
+#define KB_H_INCLUDED
+#include <map>
+#include <vector>
+#include <iterator>
+#include "Fact.h"
+#include "FactType.cpp"
+class KB{
+    protected:
+        std::map<FactType, std::vector<Fact*>> facts;//store fact into a vector with the same amount of parameters
+        std::map<std::string, FactType*> factType;//identify if there are duplicate facts with same name and same num of parameters
+        std::map<FactType, std::vector<Fact*>>::iterator myMapIterator;
+    public:
+        KB();//default constructor
+        KB(std::map<FactType, std::vector<Fact*>> facts, map<FactType, std::vector<Fact*>>::iterator myMapIterator);//constructor with params
+        Fact getFact();
+        friend class Fact;
 
 };
+
+
+
+#endif // KB_H_INCLUDED
