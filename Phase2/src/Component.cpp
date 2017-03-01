de417f797fd3b4b7aa34f1099bc8f07034b10883
sheeplegs430 <argo@Babbies-MacBook-Pro.local>
2017-03-01 12:53:45 -0800

    Adding Rule/Fact/RB/KB

---
+++ Phase2/src/Component.cpp
@@ -0,0 +1,102 @@
+//
+//  Component.cpp
+//  SRI
+//
+//  Created by Babbie Monahelis on 2/28/17.
+//  Copyright © 2017 Babbie Monahelis. All rights reserved.
+//
+
+#include "Component.hpp"
+#include <iostream>
+using namespace std;
+
+/* Methods for Component - a base class that fact and rule will derive from */
+
+//Our default constructor.
+Component::Component()
+{
+    string identity = "";
+    vector<string> elements;
+    
+}
+
+//Main constructor for our Components. This will take in a string for its
+//identity along with a vector of 'members' that represent the arguments
+//for a component.
+Component::Component(string i, vector<string> members)
+{
+    string identity = i;
+    vector<string> elements;
+  
+}
+
+//Copy constructor
+Component::Component(const Component & copyComponent)
+{
+    identity = copyComponent.identity;
+    elements = copyComponent.elements;
+}
+
+//Method for AND operator
+bool Component::AND(bool first, bool second)
+{
+    return (first && second);
+    
+}
+
+//Method for OR operator
+bool Component::OR(bool first, bool second)
+{
+    return (first || second);
+}
+
+//Deconstructor
+Component::~Component()
+{
+    
+}
+
+/************** Methods for Fact **************/
+
+
+Fact::Fact(string a, vector<string> e)
+{
+    string association = a; //Association of the fact.
+    vector<string> elements = e; //represents the members of the Fact.
+}
+
+Fact::Fact(const Fact & f)
+{
+    association = f.identity;
+    elements = f.elements;
+}
+
+Rule::Rule()
+{
+    string association = ""; //Association of the rule.
+    vector<Component*> members; //Vector of the Components that will define this rule. These can be rules or facts.
+    vector<bool> operators; //Vector to store the operators we come across in the rule.
+}
+
+Rule::Rule(string i, vector<Component*> e, vector<bool> operators)
+{
+    string association = i;
+    vector<Component*> elements = e;
+    vector<bool> function = operators;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Phase2/src/Component.hpp
@@ -0,0 +1,79 @@
+//
+//  Component.hpp
+//  SRI
+//
+//  Created by Babbie Monahelis on 2/28/17.
+//  Copyright © 2017 Babbie Monahelis. All rights reserved.
+//
+
+#ifndef Component_hpp
+#define Component_hpp
+
+#include <stdio.h>
+#include <cstdarg>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+/* This will be the base class called Component which will either be a fact or a rule. */
+
+class Component
+{
+
+public:
+    
+    vector<string> elements; //Vector of strings to hold the elements of this component.
+    string identity; //the identity for the fact or rule
+    
+    Component(); //default constructor
+    Component(string identity, vector<string> members); 
+    Component(const Component & comp); //Copy constructor.
+    Component(Component && p);
+    
+    bool find(vector<string> members); //Determines if this Component exists.
+    bool AND (bool first, bool second);
+    bool OR (bool first, bool second);
+    
+    ~Component(); //Needed to be virtual because this class has descendants.
+    
+};
+
+class Fact : Component
+{
+    
+public:
+    
+    Fact(string identity, vector<string> members); //Main constructor for a Fact.
+    Fact(const Fact & f); //Copy constructor.
+    Fact(Fact && f); //Move constructor.
+    
+    string association; //The association of the fact i.e Father, Mother, etc...
+    vector<string> members; //The members of the fact i.e John, Paul, Jones, etc...
+
+    
+   // bool factExists(vector<string> members); //Takes in a vector that represents the members of a fact.
+    
+};
+
+class Rule : Component
+{
+    
+public:
+    
+    Rule();
+    Rule(string identity, vector<Component*> elements, vector<bool> operators); //constructor that can accept rules with multiple components in a vector of type Component.
+    Rule(const Rule & r); //copy constructor.
+    Rule(Rule && r); //Move constructor.
+    
+    string association; //The association of the rule i.e Grandfather, Grandmother, Parent, etc...
+    vector<Component*> elements;
+    vector<int> functionOperator; //These point to AND/OR, similar to boolan ops.
+    
+    bool validate(vector<string> members); //Determines if a rule is valid based on the elements it is built from.
+    
+    
+};
+
+
+#endif /* Component_hpp */
